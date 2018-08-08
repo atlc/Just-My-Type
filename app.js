cd6c7d9f657e31bd352ff00999f2d1b8b7957177
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-08 12:22:53 -0500

    Roughly prototyping sentence iteration and highlighting the next needed character

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
 $('document').ready(function () {
-    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-
+    $('#yellow-block').attr('hidden', true);
     // Hides the uppercase keyboard on the loading of the document
     $('#keyboard-upper-container').attr('hidden', true);
 
+    let sentences = [
+        'ten ate neite ate nee enet ite ate inet ent eate',
+        'Too ato too nOt enot one totA not anot tOO aNot',
+        'oat itain oat tain nate eate tea anne inant nean',
+        'itant eate anot eat nato inate eat anot tain eat',
+        'nee ene ate ite tent tiet ent ine ene ete ene ate'
+    ];
+
+    function iterateThroughSentences() {
+        let sentenceChars = sentences[0].split('');
+        let id = 0;
+        sentenceChars.forEach(function(char) {
+            $('#sentence').append('<p id=\'char'+ id + '\' style=\'display: inline;\'>'+char+'</p>');
+            //$('#char'+id).addClass('highlight');
+            $('#char0').addClass('highlight');
+            id++;
+        });
+    };
+
+    function correctCharacter(char) {
+
+    };
+
+    iterateThroughSentences();
+
+
+    // When the shift key is held down, swap to the upper keyboard
     $('body').keydown(function (event) {
         let keyASCII = event.which;
-        // When the shift key is held down, swap to the upper keyboard
         if (keyASCII === 16) {
             $('#keyboard-lower-container').attr('hidden', true);
             $('#keyboard-upper-container').attr('hidden', false);
         }
     });
 
+    // When the shift key is released, swap back to the lower keyboard
     $('body').keyup(function (event) {
         let keyASCII = event.which;
-        // When the shift key is released, swap back to the lower keyboard
         if (keyASCII === 16) {
             $('#keyboard-lower-container').attr('hidden', false);
             $('#keyboard-upper-container').attr('hidden', true);
         }
     });
 
-    $('body').keypress(function(event){
+    // Highlights the key and adds animation if applicable, waits 0.15s, and then removes the class 
+    $('body').keypress(function (event) {
         let keyASCII = event.which;
         let $selector = $("[id='" + keyASCII + "']");
-        // Highlights the key and adds animation if applicable, waits 0.15s, and then removes the class 
         $($selector).addClass('keypress');
-        setTimeout(function(){ $($selector).removeClass('keypress'); }, 150);
+        setTimeout(function () { $($selector).removeClass('keypress'); }, 150);
     });
 });
